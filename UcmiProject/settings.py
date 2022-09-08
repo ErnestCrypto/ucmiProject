fed24d3c2aa42cd25c762c9b6e4a4383dd3e0170
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-09-08 23:26:53 +0000

    commit

--- UcmiProject/settings.py
+++ UcmiProject/settings.py
@@ -14,12 +14,8 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 from decouple import config
-import environ
-import os
-# Environment vatiables
-env = environ.Env()
-environ.Env.read_env()
 
+# Environment vatiables
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
